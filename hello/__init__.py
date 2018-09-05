4ac4034491ac328867da975e9558e49ce1286e95
xinghalo <xinghalo@163.com>
2018-09-05 16:32:05 +0800

    setuptools的使用

---
+++ hello/__init__.py
@@ -0,0 +1,6 @@
+def test():
+    print('Hello World!')
+
+
+if __name__ == '__main__':
+    test()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello',
+    version='1.0',
+    long_description='hello world by xingoo',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+        'argparse'
+    ],
+    test_suite='tests',
+    tests_require=['unittest2']
+)
